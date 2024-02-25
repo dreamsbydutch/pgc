7e8db94f294cc3b64e89104ee892437b2b2364e3
Curtis Hough <chough14@gmail.com>
2024-02-24 21:07:01 -0500

    comment out old leaderboard code

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -28,143 +28,145 @@ export default function Leaderboard() {
 			</>
 		)
 	}
-	if (props.data.currentTourney) {
-		return (
-			<TourneyLeaderboard
-				tourney={props.data.currentTourney}
-				allTourneys={props.data.allTourneys}
-				standings={props.data.standings}
-				live={true}
-				limit={props.limit ?? null}
-				home={props.home ?? null}
-			/>
-		)
-	} else if (props.data.nextTourney) {
-		return (
-			<>
-				<LeaderboardHeader tourney={props.data.nextTourney} allTourneys={props.data.allTourneys} />
-				{/* <CountdownLogic tourney={props.data.nextTourney} /> */}
-			</>
-		)
-	} else if (props.data.previousTourney) {
-		return (
-			<TourneyLeaderboard
-				tourney={props.data.previousTourney}
-				allTourneys={props.data.allTourneys}
-				standings={props.data.standings}
-				live={false}
-				limit={props.limit ?? null}
-				home={props.home ?? null}
-			/>
-		)
-	} else if (props.data.futureTourney) {
-		return (
-			<>
-				<LeaderboardHeader tourney={props.data.futureTourney} allTourneys={props.data.allTourneys} />
-				{/* <CountdownLogic tourney={props.data.futureTourney} /> */}
-			</>
-		)
-	}
+	return <></>
 }
+// 	if (props.data.currentTourney) {
+// 		return (
+// 			<TourneyLeaderboard
+// 				tourney={props.data.currentTourney}
+// 				allTourneys={props.data.allTourneys}
+// 				standings={props.data.standings}
+// 				live={true}
+// 				limit={props.limit ?? null}
+// 				home={props.home ?? null}
+// 			/>
+// 		)
+// 	} else if (props.data.nextTourney) {
+// 		return (
+// 			<>
+// 				<LeaderboardHeader tourney={props.data.nextTourney} allTourneys={props.data.allTourneys} />
+// 				{/* <CountdownLogic tourney={props.data.nextTourney} /> */}
+// 			</>
+// 		)
+// 	} else if (props.data.previousTourney) {
+// 		return (
+// 			<TourneyLeaderboard
+// 				tourney={props.data.previousTourney}
+// 				allTourneys={props.data.allTourneys}
+// 				standings={props.data.standings}
+// 				live={false}
+// 				limit={props.limit ?? null}
+// 				home={props.home ?? null}
+// 			/>
+// 		)
+// 	} else if (props.data.futureTourney) {
+// 		return (
+// 			<>
+// 				<LeaderboardHeader tourney={props.data.futureTourney} allTourneys={props.data.allTourneys} />
+// 				{/* <CountdownLogic tourney={props.data.futureTourney} /> */}
+// 			</>
+// 		)
+// 	}
+// }
 
-function TourneyLeaderboard(props) {
-	const [pgcEffect, setPGCEffect] = useState(false)
-	const [dbydEffect, setDbyDEffect] = useState(false)
-	// const [pgaEffect, setPGAEffect] = useState(false);
-	const [leaderboardToggle, setLeaderboardToggle] = useState(searchParams.get('tour') || 'PGC')
-	return (
-		<div className="py-4">
-			<LeaderboardHeader {...props} />
-			{props.home ? (
-				<>
-					<HomeLeaderboard {...props} />
-				</>
-			) : (
-				<div className="mt-8">
-					{props.home ? (
-						<></>
-					) : (
-						<div className="my-4 mx-auto text-center">
-							<button
-								onClick={() => {
-									setLeaderboardToggle('PGC')
-									setPGCEffect(true)
-								}}
-								className={`${pgcEffect && 'animate-toggleClick'} my-2 mx-3 py-1 px-6 rounded-lg text-lg md:text-xl sm:px-8 md:px-10 font-bold ${
-									leaderboardToggle === 'PGC' ? 'bg-gray-600 text-gray-300 shadow-btn' : 'bg-gray-300 text-gray-800 shadow-btn'
-								}`}
-								onAnimationEnd={() => setPGCEffect(false)}>
-								PGC
-							</button>
-							<button
-								onClick={() => {
-									setLeaderboardToggle('DbyD')
-									setDbyDEffect(true)
-								}}
-								className={`${dbydEffect && 'animate-toggleClick'} my-2 mx-3 py-1 px-6 rounded-lg text-lg md:text-xl sm:px-8 md:px-10 font-bold ${
-									leaderboardToggle === 'DbyD' ? 'bg-gray-600 text-gray-300 shadow-btn' : 'bg-gray-300 text-gray-800 shadow-btn'
-								}`}
-								onAnimationEnd={() => setDbyDEffect(false)}>
-								DbyD
-							</button>
-							{/* <button onClick={() => { setLeaderboardToggle("PGA"); setPGAEffect(true); }} className={`${pgaEffect && "animate-toggleClick"} my-2 mx-3 py-1 px-6 rounded-lg text-lg md:text-xl sm:px-8 md:px-10 font-bold ${leaderboardToggle === "PGA" ? "bg-gray-600 text-gray-300 shadow-btn" : "bg-gray-300 text-gray-800 shadow-btn"}`} onAnimationEnd={() => setPGAEffect(false)}>PGA</button> */}
-						</div>
-					)}
-					{leaderboardToggle === 'PGC' ? (
-						<>{/* <PGCLeaderboard {...props} /> */}</>
-					) : leaderboardToggle === 'DbyD' ? (
-						<>{/* <DbyDLeaderboard {...props} /> */}</>
-					) : (
-						<>{/* <PGALeaderboard {...props} /> */}</>
-					)}
-				</div>
-			)}
-		</div>
-	)
-}
-function HomeLeaderboard(props) {
-	const pgcLeaderboardData = props.tourney.pgcLeaderboard.filter(obj => obj.TourID === '1').slice(0, 12)
-	const dbdLeaderboardData = props.tourney.pgcLeaderboard.filter(obj => obj.TourID === '2').slice(0, 12)
-	return (
-		<div className="grid grid-flow-col grid-cols-2 text-center mx-auto mt-4">
-			<div className="border-r border-black pr-1">
-				<div className="text-lg font-bold mb-2">PGC Tour</div>
-				<div className="grid grid-flow-row grid-cols-8 text-center max-w-xl mx-auto">
-					<div className="col-span-2 text-xs font-semibold font-varela place-self-center">Rank</div>
-					<div className="col-span-4 text-sm font-semibold font-varela place-self-center">Name</div>
-					<div className="col-span-2 text-xs font-semibold font-varela place-self-center">Score</div>
-				</div>
-				{pgcLeaderboardData?.map(obj => (
-					<HomeLeaderboardItem info={obj} key={obj.Name} live={props.live} standings={props.standings?.filter(a => a.TeamName === obj.Name)[0]} />
-				))}
-			</div>
-			<div className="pl-1">
-				<div className="text-lg font-bold mb-2">DbyD Tour</div>
-				<div className="grid grid-flow-row grid-cols-8 text-center max-w-xl mx-auto">
-					<div className="col-span-2 text-xs font-semibold font-varela place-self-center">Rank</div>
-					<div className="col-span-4 text-sm font-semibold font-varela place-self-center">Name</div>
-					<div className="col-span-2 text-xs font-semibold font-varela place-self-center">Score</div>
-				</div>
-				{dbdLeaderboardData?.map(obj => (
-					<HomeLeaderboardItem info={obj} key={obj.Name} live={props.live} standings={props.standings?.filter(a => a.TeamName === obj.Name)[0]} />
-				))}
-			</div>
-		</div>
-	)
-}
+// function TourneyLeaderboard(props) {
+// 	const [pgcEffect, setPGCEffect] = useState(false)
+// 	const [dbydEffect, setDbyDEffect] = useState(false)
+// 	// const [pgaEffect, setPGAEffect] = useState(false);
+// 	const [leaderboardToggle, setLeaderboardToggle] = useState(searchParams.get('tour') || 'PGC')
+// 	return (
+// 		<div className="py-4">
+// 			<LeaderboardHeader {...props} />
+// 			{props.home ? (
+// 				<>
+// 					<HomeLeaderboard {...props} />
+// 				</>
+// 			) : (
+// 				<div className="mt-8">
+// 					{props.home ? (
+// 						<></>
+// 					) : (
+// 						<div className="my-4 mx-auto text-center">
+// 							<button
+// 								onClick={() => {
+// 									setLeaderboardToggle('PGC')
+// 									setPGCEffect(true)
+// 								}}
+// 								className={`${pgcEffect && 'animate-toggleClick'} my-2 mx-3 py-1 px-6 rounded-lg text-lg md:text-xl sm:px-8 md:px-10 font-bold ${
+// 									leaderboardToggle === 'PGC' ? 'bg-gray-600 text-gray-300 shadow-btn' : 'bg-gray-300 text-gray-800 shadow-btn'
+// 								}`}
+// 								onAnimationEnd={() => setPGCEffect(false)}>
+// 								PGC
+// 							</button>
+// 							<button
+// 								onClick={() => {
+// 									setLeaderboardToggle('DbyD')
+// 									setDbyDEffect(true)
+// 								}}
+// 								className={`${dbydEffect && 'animate-toggleClick'} my-2 mx-3 py-1 px-6 rounded-lg text-lg md:text-xl sm:px-8 md:px-10 font-bold ${
+// 									leaderboardToggle === 'DbyD' ? 'bg-gray-600 text-gray-300 shadow-btn' : 'bg-gray-300 text-gray-800 shadow-btn'
+// 								}`}
+// 								onAnimationEnd={() => setDbyDEffect(false)}>
+// 								DbyD
+// 							</button>
+// 							{/* <button onClick={() => { setLeaderboardToggle("PGA"); setPGAEffect(true); }} className={`${pgaEffect && "animate-toggleClick"} my-2 mx-3 py-1 px-6 rounded-lg text-lg md:text-xl sm:px-8 md:px-10 font-bold ${leaderboardToggle === "PGA" ? "bg-gray-600 text-gray-300 shadow-btn" : "bg-gray-300 text-gray-800 shadow-btn"}`} onAnimationEnd={() => setPGAEffect(false)}>PGA</button> */}
+// 						</div>
+// 					)}
+// 					{leaderboardToggle === 'PGC' ? (
+// 						<>{/* <PGCLeaderboard {...props} /> */}</>
+// 					) : leaderboardToggle === 'DbyD' ? (
+// 						<>{/* <DbyDLeaderboard {...props} /> */}</>
+// 					) : (
+// 						<>{/* <PGALeaderboard {...props} /> */}</>
+// 					)}
+// 				</div>
+// 			)}
+// 		</div>
+// 	)
+// }
+// function HomeLeaderboard(props) {
+// 	const pgcLeaderboardData = props.tourney.pgcLeaderboard.filter(obj => obj.TourID === '1').slice(0, 12)
+// 	const dbdLeaderboardData = props.tourney.pgcLeaderboard.filter(obj => obj.TourID === '2').slice(0, 12)
+// 	return (
+// 		<div className="grid grid-flow-col grid-cols-2 text-center mx-auto mt-4">
+// 			<div className="border-r border-black pr-1">
+// 				<div className="text-lg font-bold mb-2">PGC Tour</div>
+// 				<div className="grid grid-flow-row grid-cols-8 text-center max-w-xl mx-auto">
+// 					<div className="col-span-2 text-xs font-semibold font-varela place-self-center">Rank</div>
+// 					<div className="col-span-4 text-sm font-semibold font-varela place-self-center">Name</div>
+// 					<div className="col-span-2 text-xs font-semibold font-varela place-self-center">Score</div>
+// 				</div>
+// 				{pgcLeaderboardData?.map(obj => (
+// 					<HomeLeaderboardItem info={obj} key={obj.Name} live={props.live} standings={props.standings?.filter(a => a.TeamName === obj.Name)[0]} />
+// 				))}
+// 			</div>
+// 			<div className="pl-1">
+// 				<div className="text-lg font-bold mb-2">DbyD Tour</div>
+// 				<div className="grid grid-flow-row grid-cols-8 text-center max-w-xl mx-auto">
+// 					<div className="col-span-2 text-xs font-semibold font-varela place-self-center">Rank</div>
+// 					<div className="col-span-4 text-sm font-semibold font-varela place-self-center">Name</div>
+// 					<div className="col-span-2 text-xs font-semibold font-varela place-self-center">Score</div>
+// 				</div>
+// 				{dbdLeaderboardData?.map(obj => (
+// 					<HomeLeaderboardItem info={obj} key={obj.Name} live={props.live} standings={props.standings?.filter(a => a.TeamName === obj.Name)[0]} />
+// 				))}
+// 			</div>
+// 		</div>
+// 	)
+// }
 
-function HomeLeaderboardItem(props) {
-	const [showInfo, setShowInfo] = useState(false)
-	return (
-		<div className="border-b border-dashed border-gray-200 max-w-xl mx-auto" onClick={() => setShowInfo(!showInfo)}>
-			<div className="grid grid-flow-row grid-cols-8 text-center py-1">
-				<div className="font-varela place-self-center text-xs col-span-2 sm:text-base">{props.info.ShowRk}</div>
-				<div className="font-varela place-self-center text-sm col-span-4 sm:text-lg whitespace-nowrap">{props.info.Name}</div>
-				<div className="font-varela place-self-center text-xs col-span-2 sm:text-base">{props.info.Score}</div>
-			</div>
-		</div>
-	)
-}
+// function HomeLeaderboardItem(props) {
+// 	const [showInfo, setShowInfo] = useState(false)
+// 	return (
+// 		<div className="border-b border-dashed border-gray-200 max-w-xl mx-auto" onClick={() => setShowInfo(!showInfo)}>
+// 			<div className="grid grid-flow-row grid-cols-8 text-center py-1">
+// 				<div className="font-varela place-self-center text-xs col-span-2 sm:text-base">{props.info.ShowRk}</div>
+// 				<div className="font-varela place-self-center text-sm col-span-4 sm:text-lg whitespace-nowrap">{props.info.Name}</div>
+// 				<div className="font-varela place-self-center text-xs col-span-2 sm:text-base">{props.info.Score}</div>
+// 			</div>
+// 		</div>
+// 	)
+// }
 
 // function DropdownComponent(props) {
 //     return (
